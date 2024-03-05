b448200c97374c689535ff198784a6a4a7852dc3
Steve Kim <stevekim.0730@gmail.com>
2024-03-04 20:48:46 -0800

    3/4

--- 3.rock_paper-scissor.py
+++ 3.rock_paper-scissor.py
@@ -1,1 +1,11 @@
 import random
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        quit()
+        
+    
